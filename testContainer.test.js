72677310bba38ac2b1fe5459021aff9b89d86617
xieminhui <xmh19940228@163.com>
2018-09-30 11:45:00 +0800

    jest api

--- testContainer.test.js
+++ testContainer.test.js
@@ -69,3 +69,23 @@ forEach([0, 1], mockCallback);
 expect(mockCallback.mock.calls.length).toBe(2);
 
 console.log(mockCallback.mock);
+
+
+//api
+//describe.each
+describe.each([[1, 1, 2], [1, 2, 3], [2, 1, 3]])(
+   '.add(%i, %i)',
+   (a, b, expected) => {
+      test(`returns ${expected}`, () => {
+         expect(a + b).toBe(expected);
+      });
+      
+      test(`returned value not be greater than ${expected}`, () => {
+         expecte(a + b).not.toBeGreaterThan(expected);     
+      });
+
+      test(`returned value not be less than ${expected}`, () => {
+         expecte(a + b).not.toBeLessThan(expected);
+      });
+   }
+);
